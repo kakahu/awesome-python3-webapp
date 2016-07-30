1651d301b1e195e6e70114a19e3efe0d28209a2a
Hu Yang <kakahu1230@gmail.com>
2016-07-30 11:17:14 +0800

    day-10

--- www/apis.py
+++ www/apis.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Michael Liao'
+
+'''
+JSON API definition.
+'''
+
 import json, logging, inspect, functools
 
 class APIError(Exception):
